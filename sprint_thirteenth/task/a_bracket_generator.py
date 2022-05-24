f1723f0701976ebca484ddfe01bf9f6edab0c766
KhanovSergey <c-500-mor@yandex.ru>
2022-05-24 13:57:39 +0300

    Sprint 11 - task a solved.

---
+++ sprint_thirteenth/task/a_bracket_generator.py
@@ -0,0 +1,16 @@
+def gen_binary(count, n_one, n_two, prefix):
+    if n_one == 0 and n_two == 0:
+        print(prefix)
+    else:
+        if n_one > 0:
+            gen_binary(count + 1, n_one - 1, n_two, prefix + '(')
+        if count > 0 and n_two > 0:
+            gen_binary(count - 1, n_one, n_two - 1, prefix + ')')
+
+
+if __name__ == '__main__':
+    n = int(input())
+    count = 0
+    n_one = n
+    n_two = n
+    gen_binary(count, n_one, n_two, '')
