71c90c51c236480e212877fe06a5566eb77723cd
KhanovSergey <c-500-mor@yandex.ru>
2022-05-30 15:09:15 +0300

    Sprint 11 - task ? resolved.

--- sprint_thirteenth/task/123.py
+++ sprint_thirteenth/task/123.py
@@ -1,19 +1,52 @@
-def is_comparison(sequen, j):
-    tmp1 = sequen[j + 1] + sequen[j]
-    tmp2 = sequen[j] + sequen[j + 1]
-    if tmp1 > tmp2:
-        return True
+def merge(arr, lf, mid, rg):
+	# заводим массив для результата сортировки
+	result = [] * len(array)
+
+	# сливаем результаты
+	l, r, k = 0, 0, 0
+	while l < len(left) and r < len(right):
+		# выбираем, из какого массива забрать минимальный элемент
+		if left[l] <= right[r]:
+			result[k] = left[l]
+			l += 1
+		else:
+			result[k] = right[r]
+			r += 1
+		k += 1
+
+	# Если один массив закончился раньше, чем второй, то
+	# переносим оставшиеся элементы второго массива в результирующий
+	while l < len(left):
+		result[k] = left[l]  # перенеси оставшиеся элементы left в result
+		l += 1
+	k += 1
 
 
-def num_strength(sequen, n):
-    for i in range(n - 1):
-        for j in range(n - 1 - i):
-            if is_comparison(sequen, j):
-                sequen[j + 1], sequen[j] = sequen[j], sequen[j + 1]
-    return ''.join(sequen)
+    while r < len(right):
+	    result[k] = right[r]  # перенеси оставшиеся элементы right в result
+	    r += 1
+    k += 1
+
+    return result
 
 
-if __name__ == '__main__':
-    n = int(input())
-    sequen = input().split(' ')
-    print(num_strength(sequen, n))
+def merge_sort(arr, lf, rg):
+	if len(array) == 1:  # базовый случай рекурсии
+		return array
+
+	# запускаем сортировку рекурсивно на левой половине
+	left = merge_sort(array[0: len(array) / 2])
+
+	# запускаем сортировку рекурсивно на правой половине
+	right = merge_sort(array[len(array) / 2: len(array)])
+
+
+def test():
+	a = [1, 4, 9, 2, 10, 11]
+	b = merge(a, 0, 3, 6)
+	expected = [1, 2, 4, 9, 10, 11]
+	assert b == expected
+	c = [1, 4, 2, 10, 1, 2]
+	merge_sort(c, 0 , 6)
+	expected = [1, 1, 2, 2, 4, 10]
+	assert c == expected
